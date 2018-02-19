66f3f4fce39b78c520451bb7ceee781da3a31c79
TengLun <turn7307@gmail.com>
2018-02-19 11:00:39 -0800

    Added error logging to the logger

--- logger.go
+++ logger.go
@@ -1,16 +1,18 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 // CreateLogger creates a logger that writes to the given filename
-func CreateLogger(filename string) *log.Logger {
+func CreateLogger(filename string) (*log.Logger, error) {
 	file, err := os.OpenFile(filename+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		panic(err)
+		err = fmt.Errorf("error in creating logger file: %x", err)
+		return nil, err
 	}
 	logger := log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
-	return logger
+	return logger, nil
 }
